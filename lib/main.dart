b06ea8804ad70f98e4ffff923bfdfbdfebfb8049
joanhernandez-costa <johernandez@baufest.com>
2024-04-11 13:56:24 +0200

    Se actualizó el texto central

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Has pulsado el botón este número de veces:',
+              'Botón presionado',
             ),
             Text(
               '$_counter',
