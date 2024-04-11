e43dd970110939eebf9a2cd1ed54ba847ee230e3
joanhernandez-costa <johernandez@baufest.com>
2024-04-11 13:49:21 +0200

    Se agregó color de borde

--- lib/main.dart
+++ lib/main.dart
@@ -46,10 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
     
     return Scaffold(
       appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
+        title: Text(widget.title, style: const TextStyle(backgroundColor: Colors.blueGrey)),
       ),
       body: Center(
         
