7cebbd74fc8f3d54bdbb34a3777fb7795366b8ae
joanhernandez-costa <johernandez@baufest.com>
2024-04-11 12:42:41 +0200

    Cambio de título

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Curso Git',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Proyecto de prueba para curso Git'),
     );
   }
 }
