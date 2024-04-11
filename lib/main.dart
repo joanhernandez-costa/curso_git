71a614789c311a3d60c7bd131167f380b1473c9e
joanhernandez-costa <146813336+joanhernandez-costa@users.noreply.github.com>
2024-04-11 18:22:59 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Curso Git',
+      title: 'Curso Git y GitHub',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
